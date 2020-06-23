249875ea04c7a6a1726d556754bc453f8e8709d6
Qiang Gao <qg@Qiangs-MacBook-Air.local>
2020-06-23 15:31:47 -0500

    pushed project01 to 04_JavaScript

---
+++ 04_JavaScript/Project#1/script.js
@@ -0,0 +1,8 @@
+// alert("hello world");
+var name = prompt("Please enter your name.");
+var snack = prompt("What's your favorate snack?");
+var drink = prompt("What's your favorate drink?");
+
+welcomeMessage = "Hi there " + name + "," + " it seems that you enjoy eating " + snack + " and drinking " + drink + "!";
+
+alert(welcomeMessage);
