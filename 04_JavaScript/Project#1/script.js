1b0f66a56fd9f3d622daa15785bbfc869306d5f1
Qiang Gao <qg@Qiangs-MacBook-Air.local>
2020-06-23 17:05:59 -0500

    add id to p tag, change from getElementsByTag to getElementById

--- 04_JavaScript/Project#1/script.js
+++ 04_JavaScript/Project#1/script.js
@@ -3,6 +3,6 @@ var name = prompt("Please enter your name.");
 var snack = prompt("What's your favorate snack?");
 var drink = prompt("What's your favorate drink?");
 
-welcomeMessage = "Hi there " + name + "," + " it seems that you enjoy eating " + snack + " and drinking " + drink + "!";
+var welcomeMessage = "Hi there " + name + "," + " it seems that you enjoy eating " + snack + " and drinking " + drink + "!"
 
-alert(welcomeMessage);
+document.getElementById("p1").textContent = welcomeMessage;
