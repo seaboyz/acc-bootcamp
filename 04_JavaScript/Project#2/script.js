995f8de3dbe6e175d85010fa6f4c1815f920a639
Qiang Gao <qg@Qiangs-MacBook-Air.local>
2020-06-23 18:22:47 -0500

    add project#2

---
+++ 04_JavaScript/Project#2/script.js
@@ -0,0 +1,17 @@
+function name(){
+    var name = prompt("Please enter your name.");
+    return alert("You are going to have a wonderful day, " + name + ".")
+};
+name();
+
+function sum(){
+    var number1 = Number(prompt("Give me a number."));
+    var number2 = Number(prompt("Give me another number."))
+    return alert("By the way, the sum of your numbers is " + (number1 + number2) + ".")
+};
+sum();
+
+// var name = prompt("Please enter your name.");
+// var number1 = prompt("Give me a number.");
+// var number2 = prompt("Give me another number.")
+
