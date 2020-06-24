4a60f4c2ef8536d3238b130f5fed7cdb156c7af7
Qiang Gao <qg@Qiangs-MacBook-Air.local>
2020-06-23 19:31:22 -0500

    update

--- 04_JavaScript/Project#2/script.js
+++ 04_JavaScript/Project#2/script.js
@@ -7,11 +7,9 @@ name();
 function sum(){
     var number1 = Number(prompt("Give me a number."));
     var number2 = Number(prompt("Give me another number."))
-    return alert("By the way, the sum of your numbers is " + (number1 + number2) + ".")
+    sum = number1 + number2
 };
-sum();
+message = ("By the way, the sum of your numbers is " + sum + ".");
 
-// var name = prompt("Please enter your name.");
-// var number1 = prompt("Give me a number.");
-// var number2 = prompt("Give me another number.")
+document.getElementById("p1").textContent = message;
 
