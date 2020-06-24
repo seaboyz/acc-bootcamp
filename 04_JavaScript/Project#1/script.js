c0df2c0101935a14c3eddec7dc9cb1538caaac5a
Qiang Gao <qg@Qiangs-MacBook-Air.local>
2020-06-24 07:48:06 -0500

    use template literals instead

--- 04_JavaScript/Project#1/script.js
+++ 04_JavaScript/Project#1/script.js
@@ -3,6 +3,9 @@ var name = prompt("Please enter your name.");
 var snack = prompt("What's your favorate snack?");
 var drink = prompt("What's your favorate drink?");
 
-var welcomeMessage = "Hi there " + name + "," + " it seems that you enjoy eating " + snack + " and drinking " + drink + "!"
+// var welcomeMessage = "Hi there " + name + "," + " it seems that you enjoy eating " + snack + " and drinking " + drink + "!"
 
-document.getElementById("p1").textContent = welcomeMessage;
+// document.getElementById("p1").textContent = welcomeMessage;
+
+// Template literals (Template strings)
+document.querySelector('#p1').innerHTML = `Hi there ${name}, it seems that you enjoy eating ${snack} and drinking ${drink}!`
