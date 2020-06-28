d682f45e3a1e03ee6a54c191179760fc21a1a556
Qiang Gao <qg@Qiangs-MacBook-Air.local>
2020-06-28 13:26:18 -0500

    update

--- 04_JavaScript/Project#4/script.js
+++ 04_JavaScript/Project#4/script.js
@@ -1,18 +1,23 @@
 // testing
 // alert("Hello World!")
-// turn off all background images display
-var bgImages = document.getElementsByClassName('bg');
+
+var bgImages = document.getElementsByClassName("bg");
 var imageIndex = 0;
 function rotatingBackground() {
+    // turn off all background images display
     for (var i = 0; i < bgImages.length; i++) {
-        bgImages[i].style.display = 'none';
+        bgImages[i].style.display = "none";
     }
-    imageIndex++;
-    if (imageIndex > bgImages.length) {
-        imageIndex = 1;
-    }
-    bgImages[(imageIndex - 1)].style.display = 'block';
-}
-rotatingBackground();
-setInterval(rotatingBackground, 2000);
+    // turn background image display from none to dispaly
+    bgImages[imageIndex].style.display = "block";
 
+    imageIndex++;
+    // when imageIndex reaches bgImages number, reset it to 0
+    if (imageIndex > bgImages.length) {
+        imageIndex = 0;
+    }
+}
+// run function without waiting 2000ms
+rotatingBackground();
+// after first run, wait 2000ms , then sencond run
+setInterval(rotatingBackground, 2000);
