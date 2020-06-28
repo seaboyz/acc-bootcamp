74e668dd918a7e353a0a7c9f06d4af96979de48e
Qiang Gao <qg@Qiangs-MacBook-Air.local>
2020-06-28 13:17:55 -0500

    finish project4

--- 04_JavaScript/Project#4/script.js
+++ 04_JavaScript/Project#4/script.js
@@ -1,2 +1,18 @@
 // testing
 // alert("Hello World!")
+// turn off all background images display
+var bgImages = document.getElementsByClassName('bg');
+var imageIndex = 0;
+function rotatingBackground() {
+    for (var i = 0; i < bgImages.length; i++) {
+        bgImages[i].style.display = 'none';
+    }
+    imageIndex++;
+    if (imageIndex > bgImages.length) {
+        imageIndex = 1;
+    }
+    bgImages[(imageIndex - 1)].style.display = 'block';
+}
+rotatingBackground();
+setInterval(rotatingBackground, 2000);
+
