845920d32abc4c11b9b71848ea2022a9d310dac8
Qiang Gao <qg@Qiangs-MacBook-Air.local>
2020-06-29 22:02:48 -0500

    update

--- Practice/Todo List/script.js
+++ Practice/Todo List/script.js
@@ -1,7 +1,8 @@
 var itemList = document.getElementById("items");
+var form = document.getElementById("addForm");
+var filter = document.getElementById('filter');
 
 // Form submit event
-var form = document.getElementById("addForm");
 form.addEventListener("submit", addItem);
 
 // Delete button event
@@ -16,6 +17,43 @@ itemList.addEventListener("click", removeItem);
 //     delButtonList[i].addEventListener('click', removeItem);
 // }
 
+// filter event
+filter.addEventListener('keyup', filterItem);
+
+
+// Add item
+function addItem(e) {
+    e.preventDefault();
+    
+    // Get input value
+    var newItem = document.getElementById("item").value;
+    
+    // Create new li element
+    var li = document.createElement("li");
+    // Add class
+    li.className = "list-group-item";
+    // Add text node with input value
+    newItemText = document.createTextNode(newItem);
+    // append newItemText to text node
+    li.appendChild(newItemText);
+    
+    // Create del button element
+    var delButton = document.createElement("button");
+    // Add Classes to delButton
+    delButton.className = "btn btn-danger btn-sm float-right delete";
+    // Append text node
+    delButton.appendChild(document.createTextNode("X"));
+    
+    // Append delete button to li
+    li.appendChild(delButton);
+    
+    // Append li element to itemList
+    itemList.appendChild(li);
+
+    // Delete the input content 
+    console.log(newItem);
+}
+
 // Remove item
 function removeItem(e) {
     // Using class to distinguish if delete button is clicked.
@@ -28,32 +66,29 @@ function removeItem(e) {
     }
 }
 
-// Add item
-function addItem(e) {
-    e.preventDefault();
-
-    // Get input value
-    var newItem = document.getElementById("item").value;
-
-    // Create new li element
-    var li = document.createElement("li");
-    // Add class
-    li.className = "list-group-item";
-    // Add text node with input value
-    newItemText = document.createTextNode(newItem);
-    // append newItemText to text node
-    li.appendChild(newItemText);
-
-    // Create del button element
-    var delButton = document.createElement("button");
-    // Add Classes to delButton
-    delButton.className = "btn btn-danger btn-sm float-right delete";
-    // Append text node
-    delButton.appendChild(document.createTextNode("X"));
-
-    // Append delete button to li
-    li.appendChild(delButton);
-
-    // Append li element to itemList
-    itemList.appendChild(li);
+// filter items
+function filterItem(e) {
+    // convert text typed in to lowercase
+    var text = e.target.value.toLowerCase();
+    // Get list
+    var items = itemList.getElementsByTagName('li');
+    // Convert to an array
+    items = Array.from(items);
+    // Get text content for each item and convert it to lower case
+    for(var i = 0; i < items.length; i++){
+        var itemName = items[i].firstChild.textContent.toLowerCase();
+        // compare item name to the search box text
+        // The indexOf() method returns the position of the first occurrence of a specified value in a string.
+        // This method returns -1 if the value to search for never occurs.
+        // Note: The indexOf() method is case sensitive.
+        if(itemName.indexOf(text) != -1){
+            items[i].style.display = 'block';
+        }else{
+            items[i].style.display = 'none';
+        }
+        // console.log(itemName);
+    }
+    // Array.from(items).forEach(function(item){
+    //     var itemName = item.firstChild.textContent;
+    // })
 }
