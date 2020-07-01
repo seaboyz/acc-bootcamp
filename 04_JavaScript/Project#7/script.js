d67fe705d2b4f494b293a25d82c4f04041c12f25
Qiang Gao <qg@Qiangs-MacBook-Air.local>
2020-06-30 22:54:49 -0500

    finish project 7

--- 04_JavaScript/Project#7/script.js
+++ 04_JavaScript/Project#7/script.js
@@ -1,1 +1,20 @@
-console.log("hello");
+var submit = document.getElementById('buttonSubmit');
+
+// submit event
+submit.addEventListener('click', checkEmpty);
+
+function checkEmpty(e) {
+    e.preventDefault();
+    // get email input value
+    var inputEmail = document.getElementById('inputEmail').value;
+    // get password input value
+    var inputPassword = document.getElementById('inputPassword').value;
+    // check empty
+    if ((inputEmail == '') || (inputPassword == '')){
+        alert('Please check the fields and make sure they are not blank.');
+    } else{
+        // Delete the input content 
+        document.getElementById('inputEmail').value = '';
+        document.getElementById('inputPassword').value = '';
+    }
+}
