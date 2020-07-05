238b23c994dc852a8294f3813eb449eb72ebe244
Qiang Gao <qg@Qiangs-MacBook-Air.local>
2020-07-04 20:22:00 -0500

    update

--- 04_JavaScript/Project#7/script.js
+++ 04_JavaScript/Project#7/script.js
@@ -5,16 +5,17 @@
     // submit event
     submit.addEventListener('click', checkEmpty);
 
-    function checkEmpty(e) {
-        e.preventDefault();
+    function checkEmpty() {
+        // preventDefault();
         // get email input value
         var inputEmail = document.getElementById('inputEmail').value;
         // get password input value
         var inputPassword = document.getElementById('inputPassword').value;
         // check empty
-        if ((inputEmail == '') || (inputPassword == '')){
+        if ((inputEmail === '') || (inputPassword === '')){
             alert('Please check the fields and make sure they are not blank.');
         } else{
+            console.log('Congratulations!')
             // Delete the input content 
             document.getElementById('inputEmail').value = '';
             document.getElementById('inputPassword').value = '';
