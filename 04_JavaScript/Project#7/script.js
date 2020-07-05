415d9b1fa6304d7e10ce1a7358b4141dd97450d9
Qiang Gao <qg@Qiangs-MacBook-Air.local>
2020-07-04 19:14:22 -0500

    change to Immediate Invoded Function Expression

--- 04_JavaScript/Project#7/script.js
+++ 04_JavaScript/Project#7/script.js
@@ -1,20 +1,25 @@
-var submit = document.getElementById('buttonSubmit');
+(function check(e) {
+    
+    var submit = document.getElementById('buttonSubmit');
 
-// submit event
-submit.addEventListener('click', checkEmpty);
+    // submit event
+    submit.addEventListener('click', checkEmpty);
 
-function checkEmpty(e) {
-    e.preventDefault();
-    // get email input value
-    var inputEmail = document.getElementById('inputEmail').value;
-    // get password input value
-    var inputPassword = document.getElementById('inputPassword').value;
-    // check empty
-    if ((inputEmail == '') || (inputPassword == '')){
-        alert('Please check the fields and make sure they are not blank.');
-    } else{
-        // Delete the input content 
-        document.getElementById('inputEmail').value = '';
-        document.getElementById('inputPassword').value = '';
+    function checkEmpty(e) {
+        e.preventDefault();
+        // get email input value
+        var inputEmail = document.getElementById('inputEmail').value;
+        // get password input value
+        var inputPassword = document.getElementById('inputPassword').value;
+        // check empty
+        if ((inputEmail == '') || (inputPassword == '')){
+            alert('Please check the fields and make sure they are not blank.');
+        } else{
+            // Delete the input content 
+            document.getElementById('inputEmail').value = '';
+            document.getElementById('inputPassword').value = '';
+        }
     }
-}
+})();
+
+
