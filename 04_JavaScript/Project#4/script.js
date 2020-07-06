7063bef99e670818d6ddab523f5977eca453c759
Qiang Gao <qg@Qiangs-MacBook-Air.local>
2020-07-06 13:30:49 -0500

    change to IIFE

--- 04_JavaScript/Project#4/script.js
+++ 04_JavaScript/Project#4/script.js
@@ -1,23 +1,24 @@
-// testing
-// alert("Hello World!")
 
-var bgImages = document.getElementsByClassName("bg");
-var imageIndex = 0;
-function rotatingBackground() {
-    // turn off all background images display
-    for (var i = 0; i < bgImages.length; i++) {
-        bgImages[i].style.display = "none";
-    }
-    
-    imageIndex++;
-    // when imageIndex reaches bgImages number, reset it to 0
-    if (imageIndex > bgImages.length) {
-        imageIndex = 1;
-    }
-    // turn background image display from none to dispaly
-    bgImages[imageIndex-1].style.display = "block";
-}
-// run function without waiting 2000ms
-rotatingBackground();
-// after first run, wait 2000ms , then sencond run
-setInterval(rotatingBackground, 2000);
+
+(function rotatingBackground() {
+
+    var bgImages = document.getElementsByClassName("bg");
+    var imageIndex = 0;
+
+    setInterval(function() {
+        for (var i = 0; i < bgImages.length; i++) {
+            bgImages[i].style.display = "none";
+        }
+        imageIndex++;
+        // when imageIndex reaches bgImages number, reset it to 0
+        if (imageIndex > bgImages.length) {
+            imageIndex = 1;
+        }
+        // turn background image display from none to dispaly
+        bgImages[imageIndex-1].style.display = "block";
+
+        }, 1000);
+       
+}());
+
+
