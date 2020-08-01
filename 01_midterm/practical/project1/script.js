1f7dd5b2e08acb5bb173f3818dedcc7d8bfe6099
seaboyz <seaboyz@hotmail.com>
2020-08-01 16:19:01 -0500

    Finished project 1

--- 01_midterm/practical/project1/script.js
+++ 01_midterm/practical/project1/script.js
@@ -1,4 +1,17 @@
-$("lbsInput").change(function(){
-    console.log($("lbsInput").val())
-    // $("kgInput").val()
+let lbs = document.getElementById("lbsInput");
+let kg = document.getElementById("kgInput");
+let miles = document.getElementById("milesInput");
+let km = document.getElementById("kmInpunt");
+
+lbs.addEventListener("keyup", function() {
+    kg.value = (lbs.value * 0.45).toFixed(3)
+})
+kg.addEventListener("keyup", function() {
+    lbs.value = (kg.value * 2.2).toFixed(3)
+})
+miles.addEventListener("keyup", function() {
+    km.value = (miles.value * 1.6).toFixed(3)
+})
+km.addEventListener("keyup", function() {
+    miles.value = (km.value * 0.62).toFixed(3)
 })
