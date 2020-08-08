bc1aa4a81c4aec2fa2c2e4e2f2f5adc82264748d
seaboyz <seaboyz@hotmail.com>
2020-08-07 21:18:32 -0500

    Add ejsZoo to the Practice

---
+++ Practice/ejsZoo/zoo.js
@@ -0,0 +1,76 @@
+const zoo = [
+    {
+        name: "Tik-tok",
+        breed: 'alligator',
+        size: 'large',
+        isPet: false,
+        country: 'Neverland',
+        owner: {
+            fname: 'Peter',
+            lname: 'Pan',
+            phone: '999-HOOK'
+        }
+    },
+    {
+        name: "Melman",
+        breed: 'giraffe',
+        size: 'x-large',
+        isPet: false,
+        country: 'Madagascar',
+        owner: {
+            fname: 'Vitaly',
+            lname: 'Zaragoza',
+            phone: '512-RICO'
+        }
+    },
+    {
+        name: "Mickey",
+        breed: 'mouse',
+        size: 'x-small',
+        isPet: true,
+        country: 'U.S.',
+        owner: {
+            fname: 'Walt',
+            lname: 'Disney',
+            phone: '555-EARS'
+        }
+    },
+    {
+        name: "Bruce",
+        breed: 'hyena',
+        size: 'medium',
+        isPet: true,
+        country: 'U.S',
+        owner: {
+            fname: 'Harleen',
+            lname: 'Quinzel',
+            phone: '12J-OKER'
+        }
+    },
+    {
+        name: "Squeak",
+        breed: 'bat',
+        size: 'small',
+        isPet: true,
+        country: 'Transylvania',
+        owner: {
+            fname: 'Vlad',
+            lname: 'Tempus',
+            phone: 'TR6-5000'
+        }
+    },
+    {
+        name: "Francis",
+        breed: 'lady bug',
+        size: 'x-x-small',
+        isPet: false,
+        country: 'Your Backyard',
+        owner: {
+            fname: 'P.T.',
+            lname: 'Flea',
+            phone: 'BUG-LIFE'
+        }
+    }
+]
+
+module.exports = zoo
