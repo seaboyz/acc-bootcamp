9be527b29a42bd474738688d21976bb1c1e1bae2
seaboyz <seaboyz@hotmail.com>
2020-08-12 21:46:20 -0500

    Add LYRICS to practice

---
+++ Practice/LYRICS/server.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const fetch = require("node-fetch");
+const { response } = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+    res.render("home.ejs");
+});
+
+app.get("/lyricResults", (req, res) => {
+    // console.log(req.query.artist)
+    let artist = req.query.artist;
+    let title = req.query.title;
+    let url = `https://api.lyrics.ovh/v1/${artist}/${title}`;
+
+    fetch(url)
+    .then(response => {
+        if (!response) {
+            throw Error("no response");
+        }
+        return response.json();
+    })
+    .then((data)=>{
+        let lyrics = data.lyrics
+        res.render("lyricResults.ejs", {lyrics:lyrics})
+    })
+    .watch()
+
+    res.render("lyricResults.ejs");
+});
+
+PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`App is listening on port ${PORT}`);
+});
