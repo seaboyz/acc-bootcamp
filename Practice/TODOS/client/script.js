51e33bdc29e89e05f879dcec6ae787b0425d7257
seaboyz <seaboyz@hotmail.com>
2020-08-19 21:37:56 -0500

    Add TODOS to Practice

---
+++ Practice/TODOS/client/script.js
@@ -0,0 +1,25 @@
+// NOTE: Original trial class code
+$("input").keypress(function(event) {
+  if (event.which === 13 && $(this).val() !== "") {
+    var todoItem = $(this).val();
+    $("ul").append(
+      "<li>" +
+        todoItem +
+        "<span>" +
+        "<i class='far fa-trash-alt'></i>" +
+        "</span>" +
+        "</li>"
+    );
+    $("input").val("");
+  }
+});
+
+$("ul").on("click", "li", function() {
+  $(this).toggleClass("completed");
+});
+
+$("ul").on("click", "span", function(event) {
+  $(this)
+    .parent()
+    .remove();
+});
---
+++ Practice/TODOS/server/server.js
@@ -0,0 +1,76 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const logger = require("morgan");
+
+const app = express();
+
+app.use(logger("dev"));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+let toDoArray = [
+	{
+		id: 1,
+		description: "Call mom",
+		isComplete: "false",
+	},
+	{
+		id: 2,
+		description: "Buy groceries",
+		isComplete: "false",
+	},
+	{
+		id: 3,
+		description: "Go to movies",
+		isComplete: "false",
+	},
+];
+
+let newTodo = {
+	id: 4,
+	description: "Buy more stuff",
+	isComplete: "false",
+};
+
+app.get("/", (req, res) => {
+	res.send("<h1>Home</h1>");
+});
+app.get("/todos", (req, res) => {
+	res.json(toDoArray);
+});
+
+app.post("/todos", (req, res) => {
+	toDoArray.push(newTodo);
+	res.status(201).json(newTodo);
+});
+
+app.delete("/todos/:todoid", (req, res) => {
+	let id = parseInt(req.params.todoid);
+	toDoArray = toDoArray.filter((todo) => todo.id !== id);
+	res.status(201).json(toDoArray);
+});
+
+app.put("/todos/:todoid", (req, res) => {
+	let id = parseInt(req.params.todoid);
+	toDoArray.forEach((todo) => {
+		if (todo.id === id) {
+			todo.isComplete = !todo.isComplete;
+		}
+	});
+
+	res.status(201).json(toDoArray);
+});
+
+// app.delete("/todos/:id", (req, res) => {
+// 	let id = req.params.id
+// 	console.log(id)
+// 	// let index = toDoArray.indexOf(newTodo);
+// 	// 	toDoArray.splice(index, 1);
+// 	// res.redirect("/");
+// });
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+	console.log(`App is listening on PORT ${PORT}.`);
+});
