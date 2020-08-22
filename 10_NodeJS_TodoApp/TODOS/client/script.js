a61e860dee46f04f249a1695297e3805a01c11cc
seaboyz <seaboyz@hotmail.com>
2020-08-22 00:11:44 -0500

    Finish READ and CREATE for frontend

--- 10_NodeJS_TodoApp/TODOS/client/script.js
+++ 10_NodeJS_TodoApp/TODOS/client/script.js
@@ -1,26 +1,66 @@
 // NOTE: Original trial class code
-$("input").keypress(function(event) {
-  if (event.which === 13 && $(this).val() !== "") {
-    var todoItem = $(this).val();
-    $("ul").append(
-      "<li>" +
-        todoItem +
-        "<span>" +
-        "<i class='far fa-trash-alt'></i>" +
-        "</span>" +
-        "</li>"
-    );
-    $("input").val("");
-  }
+
+$("ul").on("click", "li", function () {
+	$(this).toggleClass("completed");
+	console.log($(this));
 });
 
-$("ul").on("click",/* "li", */ function() {
-  $(this).toggleClass("completed");
-  console.log($(this))
+$("ul").on("click", "span", function (event) {
+	$(this)
+		//.parent()
+		.remove();
 });
 
-$("ul").on("click", "span", function(event) {
-  $(this)
-    //.parent()
-    .remove();
+const todoUrl = "http://localhost:3000/todos";
+
+// READ
+$(document).ready(() => {
+	$.ajax({
+		method: "GET",
+		url: todoUrl,
+	})
+		.done((data) => {
+			$("li").remove();
+			data.forEach((todo) => {
+				$("ul").append(
+					"<li>" +
+						todo.description +
+						"<span>" +
+						"<i class='far fa-trash-alt'></i>" +
+						"</span>" +
+						"</li>"
+				);
+			});
+		})
+		.fail((error) => {
+			console.error("Issues getting data from");
+		});
+});
+
+// CREATE
+$("input").keypress(function (event) {
+	if (event.which === 13 && $(this).val() !== "") {
+		let todoItem = {
+			description: $(this).val(),
+		};
+		$.ajax({
+			url: todoUrl,
+			method: "POST",
+			data: todoItem,
+		})
+			.done((data) => {
+				$("ul").append(
+					"<li>" +
+						data.description +
+						"<span>" +
+						"<i class='far fa-trash-alt'></i>" +
+						"</span>" +
+						"</li>"
+				);
+				$("input").val("");
+			})
+			.fail((error) => {
+				console.error("Failed getting new todo created in server", err);
+			});
+	}
 });
--- 10_NodeJS_TodoApp/TODOS/server/server.js
+++ 10_NodeJS_TodoApp/TODOS/server/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const logger = require("morgan");
+const cors = require("cors");
 
 const app = express();
 
@@ -8,6 +9,8 @@ app.use(logger("dev"));
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
+app.use(cors());
 
 let toDoArray = [
 	{
@@ -26,24 +29,26 @@ let toDoArray = [
 		isComplete: "false",
 	},
 ];
-
-let newTodo = {
-	id: 4,
-	description: "Buy more stuff",
-	isComplete: "false",
-};
+let count = 4;
 
 app.get("/", (req, res) => {
 	res.send("<h1>Home</h1>");
 });
+// READ
 app.get("/todos", (req, res) => {
 	res.json(toDoArray);
 });
-
+// CREATE
 app.post("/todos", (req, res) => {
+	let newTodo = {
+		id: count++,
+		description: req.body.description,
+		isComplete: "false",
+	};
 	toDoArray.push(newTodo);
 	res.status(201).json(newTodo);
 });
+// DELETE
 
 app.delete("/todos/:todoid", (req, res) => {
 	let id = parseInt(req.params.todoid);
@@ -54,7 +59,7 @@ app.delete("/todos/:todoid", (req, res) => {
 app.put("/todos/:todoid", (req, res) => {
 	let idRequested = parseInt(req.params.todoid);
 	toDoArray.forEach((todo) => {
-		if (todo.id===idRequested) {
+		if (todo.id === idRequested) {
 			todo.isComplete = !todo.isComplete;
 		}
 	});
@@ -62,13 +67,13 @@ app.put("/todos/:todoid", (req, res) => {
 	res.status(201).json(toDoArray);
 });
 
-// app.delete("/todos/:id", (req, res) => {
-// 	let id = req.params.id
-// 	console.log(id)
-// 	// let index = toDoArray.indexOf(newTodo);
-// 	// 	toDoArray.splice(index, 1);
-// 	// res.redirect("/");
-// });
+app.delete("/todos/:id", (req, res) => {
+	let id = req.params.id;
+	console.log(id);
+	let index = toDoArray.indexOf(newTodo);
+	toDoArray.splice(index, 1);
+	res.status(201).json(toDoArray);
+});
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
