0b290991994ddac57b0203ae191837aa3f696835
seaboyz <seaboyz@hotmail.com>
2020-09-01 00:53:44 -0500

    Finish project3 todos

--- 12_Mongo/Prpject 3/TODOS/client/script.js
+++ 12_Mongo/Prpject 3/TODOS/client/script.js
@@ -2,43 +2,22 @@
 
 const todoUrl = "http://localhost:3000/todos";
 
-// READ
-$(document).ready(() => {
-	$.ajax({
-		method: "GET",
-		url: todoUrl,
-	})
-		// get data from server
-		.done((toDoArray) => {
-			// remove all the fake todo items
-			$("li").remove();
-			toDoArray.forEach((todo) => {
-				$("ul").append(
-					// asign an id for each list item using the id from the server as data attribute
-					`<li data-id=${todo.id}>
-						${todo.description}
-						<span><i class='far fa-trash-alt'></i></span>
-					</li>`
-				);
-			});
-		})
-		.fail((error) => {
-			console.error("Issues getting data from server");
-		});
-});
-
 // CREATE
+// on keypress triger a call back function,using keypress event as parameter
 $("input").keypress(function (event) {
+	// if the press enter key and the input value is not empty
 	if (event.which === 13 && $(this).val() !== "") {
 		let todoItem = { description: $(this).val() };
+		// send input value to sever use post method
 		$.ajax({
 			url: todoUrl,
 			method: "POST",
 			data: todoItem,
 		})
+			// receive data from server
 			.done((newtodo) => {
 				$("ul").append(
-					`<li data-id=${newtodo.id}>
+					`<li data-id=${newtodo._id}>
 						${newtodo.description}
 						<span><i class='far fa-trash-alt'></i></span>
 					</li>`
@@ -51,17 +30,52 @@ $("input").keypress(function (event) {
 	}
 });
 
-// PUT
+// READ
+// when page finishs loading
+$(document).ready(() => {
+	// request data from server
+	$.ajax({
+		method: "GET",
+		url: todoUrl,
+	})
+		// receive data from server
+		.done((toDoArray) => {
+			// remove all the fake todo items
+			$("li").remove();
+			toDoArray.forEach((todo) => {
+				let completed = todo.isComplete ? "completed" : "";
+				$("ul").append(
+					// asign an id for each list item using the _id from the server as data attribute
+					// show completed status
+					`<li class="${completed}" data-id=${todo._id}>
+						${todo.description}
+						<span><i class='far fa-trash-alt'></i></span>
+					</li>`
+				);
+			});
+		})
+		// if not recieve data from server
+		.fail((error) => {
+			console.error("Issues getting data from server");
+		});
+});
+
+// Update
+// when click a list item, trigger a callback funtion
 $("ul").on("click", "li", function () {
 	// get data attribute from the clicked item
 	let id = $(this).data("id");
+	// send put request to server
 	$.ajax({
+		// send user id through url to server
 		method: "PUT",
 		url: `${todoUrl}/${id}`,
 	})
+		// receive data from server
 		.done((requestedToDo) => {
-			let id = requestedToDo.id;
+			let id = requestedToDo._id;
 			// data atrribute selector
+			// toggle completed class
 			$(`li[data-id="${id}"]`).toggleClass("completed");
 		})
 		.fail((error) => {
@@ -70,15 +84,17 @@ $("ul").on("click", "li", function () {
 });
 
 // DELETE
-$("ul").on("click", "span", function () {
+$("ul").on("click", "span", function (event) {
+	event.stopPropagation();
 	// get data attribute from the clicked item
 	let id = $(this).parent().data("id");
+	// send delete request to server
 	$.ajax({
 		method: "DELETE",
 		url: `${todoUrl}/${id}`,
 	})
 		.done((requestedToDo) => {
-			let id = requestedToDo.id;
+			let id = requestedToDo._id;
 			$(`li[data-id="${id}"]`).remove();
 		})
 		.fail((error) => {
--- 12_Mongo/Prpject 3/TODOS/server/server.js
+++ 12_Mongo/Prpject 3/TODOS/server/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 const logger = require("morgan");
 const cors = require("cors");
 
@@ -12,69 +13,114 @@ app.use(bodyParser.json());
 
 app.use(cors());
 
-let toDoArray = [
-	{
-		id: 1,
-		description: "Call mom",
-		isComplete: false,
+// Connection
+const db = "todo_list";
+const url = `mongodb://localhost:27017/${db}`;
+
+mongoose
+	.connect(url, {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+		useFindAndModify: false,
+	})
+	.then(() => console.log(`Connected to ${db} database`))
+	.catch(() => console.log(`Error connecting to ${db} database `, error));
+
+// BluePrints
+let todoSchema = new mongoose.Schema({
+	description: {
+		type: String,
+		required: [true, "Must have a description"],
 	},
-	{
-		id: 2,
-		description: "Buy groceries",
-		isComplete: false,
+	isComplete: {
+		type: Boolean,
+		default: false,
 	},
-	{
-		id: 3,
-		description: "Go to movies",
-		isComplete: false,
-	},
-];
-// new todo starter number
-let count = 4;
+});
+
+let TodoModel = mongoose.model("todos", todoSchema);
+
 // HOME ROUTE
 app.get("/", (req, res) => {
 	res.send("<h1>Home</h1>");
 });
 
-// READ
-app.get("/todos", (req, res) => {
-	res.json(toDoArray);
-});
-
 // CREATE
+// recieve data from cliet using post request
 app.post("/todos", (req, res) => {
-	let newTodo = {
-		id: count++,
+	let newTodo = new TodoModel({
 		description: req.body.description,
-		isComplete: false,
-	};
-	toDoArray.push(newTodo);
-	res.status(201).json(newTodo);
+	});
+	// save data to database
+	newTodo.save((error, results) => {
+		if (error) {
+			console.log("There was an error saving to db");
+		} else {
+			console.log("Data is suceessfully saved:", results);
+			// if save data to database, then
+			// send data to client
+			res.status(201).json(newTodo);
+		}
+	});
 });
 
-// PUT
-app.put("/todos/:todoid", (req, res) => {
-	let requestedToDoId = parseInt(req.params.todoid);
-	let requestedToDo = toDoArray.find((todo) => {
-		return (todo.id = requestedToDoId);
+// READ
+// when there is get request from client
+app.get("/todos", (req, res) => {
+	// request data from database
+	TodoModel.find({}, (error, results) => {
+		// if can not receive data from database
+		if (error) {
+			console.log("Error finding documents from database: ", error);
+			// receive data from database
+		} else {
+			// send data to client
+			res.json(results);
+		}
 	});
-	// toggle the completion status
-	requestedToDo.isComplete = !requestedToDo.isComplete;
-	res.status(201).json(requestedToDo);
+});
+
+// Update
+// recieve put request from client
+app.put("/todos/:todoid", (req, res) => {
+	// recieve id from url
+	let requestedToDoId = req.params.todoid;
+	TodoModel.findById(requestedToDoId, (error, result) => {
+		if (error) {
+			console.log("Error finding documents from database: ", error);
+			// receive data from database
+		} else {
+			// toggole isComplete field
+			result.isComplete = !result.isComplete;
+			// save result to server
+			result.save((error, result) => {
+				if (error) {
+					console.log("There was an error saving to db");
+				} else {
+					console.log("Data is suceessfully saved:", result);
+					// if save data to database, then
+					// send data to client
+					res.status(201).json(result);
+				}
+			});
+		}
+	});
 });
 
 // DELETE
 app.delete("/todos/:todoid", (req, res) => {
-	let requestedToDoId = parseInt(req.params.todoid);
-	let requestedToDo = toDoArray.find((todo) => {
-		return (todo.id = requestedToDoId);
+	let requestedToDoId = req.params.todoid;
+	// send delete request to database
+	TodoModel.findByIdAndDelete(requestedToDoId, (error, result) => {
+		if (error) {
+			console.log("Error finding documents from database: ", error);
+			// receive data from database
+		} else {
+			// send result to client
+			res.status(201).json(result);
+		}
 	});
-	let indexOfRequestedToDo = toDoArray.indexOf(requestedToDo)
-	// delete the requestedTodo from todoArray
-	toDoArray.splice(indexOfRequestedToDo,1)
-	res.status(201).json(requestedToDo);
 });
-
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
