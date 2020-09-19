704502768ddb08952ec814487d48f8cd42146a2b
seaboyz <seaboyz@hotmail.com>
2020-09-18 23:44:47 -0500

    Finished todosWithSql

--- 13_SQL/Project2/todosWithSql/server/server.js
+++ 13_SQL/Project2/todosWithSql/server/server.js
@@ -1,4 +1,3 @@
-const pgdb = require("pgdb");
 const express = require("express");
 const bodyParser = require("body-parser");
 const logger = require("morgan");
@@ -23,193 +22,99 @@ const client = new Client({
 	port: 5432,
 });
 
-// CALL BACK
+client
+	.connect()
+	.then(() =>
+		console.log(`Connected to ${client.database} on port ${client.port}`)
+	)
+	.catch(err => console.log(`Error connecting: ${err.stack}`));
 
-// client
-// 	.connect()
-// 	.then(() => console.log("Connected successfully"))
-// 	.then(() => client.query("SELECT * FROM users"))
-// 	.then(results => console.table(results.rows))
-// 	.catch(err => console.log(err))
-// 	.finally(() => client.end());
+app.get("/", (req, res) => {
+	res.send(`This is the Todos API running on PORT ${PORT}.`);
+});
 
-// // ASYNC AWAIT
+// CRUD
+// CREATE
+// recieve data from cliet using post request
+app.post("/todos", (req, res) => {
+	// create a document using the input as the the description field value
+	let description = req.body.description;
+	let query = `
+			INSERT INTO todos.todos 
+				(description, iscomplete, user_id)
+			VALUES
+				('${description}', false, 8)
+			RETURNING id as _id, *;
+		`;
+	client
+		.query(query)
+		.then(function (data) {
+			res.json(data.rows[0]);
+		})
+		.catch(function (err) {
+			res.json({ code: 400, message: err.stack, err });
+		});
+});
 
-// (async () => {
-// 	try {
-// 		await client.connect();
-// 		console.log("Conneted successfully.");
-// 		// destructuring
-// 		const {rows} = await client.query("SELECT * FROM users");
-// 		// const results = await client.query("SELECT * FROM users");
-// 		console.table(rows);
-// 		// console.table(results.rows);
+// READ
+// when there is get request from client's ajax
+app.get("/todos", (req, res) => {
+	// request data from database
+	let query = `
+			SELECT id as _id, * 
+			FROM todos.todos
+			WHERE user_id = 8;
+		`;
 
-// 	} catch (err) {
-// 		console.log("Somthing wrong happend", err);
-// 	} finally {
-// 		await client.end();
-// 		console.log("Client discunnected successfully.");
-// 	}
-// })();
-
-// TRANSACTION
-
-(async () => {
-	try {
-		await client.connect();
-		await client.query("BEGIN");
-		await client.query("INSERT INTO exployees");
-		console.log("Inserted a new row");
-		await client.query("COMMIT");
-	} catch (err) {
-		console.log("Failed to execute someth,", err);
-		await client.query("ROLLBACK");
-	} finally {
-		await client.end();
-		console.log("Cleaned");
-	}
-})();
-
-// // database url
-// // "todo_list" is the name of the database
-// const db = "todo_list";
-// const url = `mongodb://localhost:27017/${db}`;
-
-// // connection
-// // when conntect url succeffuly, mongodb create a database call "todo_list"
-// mongoose
-// 	// connect to url
-// 	.connect(url, {
-// 		useNewUrlParser: true,
-// 		useUnifiedTopology: true,
-// 		useFindAndModify: false,
-// 	})
-
-// 	// if connection to database is successfull
-// 	.then(() => console.log(`Connected to ${db} database`))
-
-// 	// if connection to database is wrong
-// 	.catch(() => console.log(`Error connecting to ${db} database `, error));
-
-// // defining schema
-// let Schema = mongoose.Schema;
-
-// let todoSchema = new Schema({
-// 	description: {
-// 		type: String, // "String" is the shorthand of "type: String"
-// 		// validation set description field must have a string value
-// 		required: [true, "Must have a description"],
-// 	},
-// 	isComplete: {
-// 		type: Boolean,
-// 		// setup the default value for isComplete field
-// 		default: false,
-// 	},
-// 	createdAt: {
-// 		type: Date,
-// 		default: Date.now,
-// 	},
-// });
-// // create model
-// // when create a mongoose model, mongo creates a collection called todos
-// // let TodoModel = mongoose.model("todos", todoSchema);
-// // a altertive way create a model, the mongoose will convert "Todo" to "todos" as collection name
-
-// let TodoModel = mongoose.model("Todo", todoSchema);
-
-// // HOME ROUTE
-// app.get("/", (req, res) => {
-// 	res.send("<h1>Home</h1>");
-// });
-
-// // CRUD
-// // CREATE
-// // recieve data from cliet using post request
-// app.post("/todos", (req, res) => {
-// 	// create a document using the input as the the description field value
-// 	let newTodo = new TodoModel({
-// 		description: req.body.description,
-// 	});
-// 	// save data to database
-// 	newTodo.save((error, results) => {
-// 		if (error) {
-// 			console.log("There was an error saving to db");
-// 		} else {
-// 			console.log("Data is suceessfully saved:", results);
-// 			// if save data to database, then
-// 			// send data to client
-// 			res.status(201).json(newTodo);
-// 		}
-// 	});
-// });
-
-// // READ
-// // when there is get request from client's ajax
-// app.get("/todos", (req, res) => {
-// 	// request data from database
-// 	TodoModel.find({}, (error, results) => {
-// 		// if can not receive data from database
-// 		if (error) {
-// 			console.log("Error finding documents from database: ", error);
-// 			// receive data from database
-// 		} else {
-// 			// send data to client ajax
-// 			console.log(results);
-// 			console.table(results);
-// 			res.json(results);
-// 		}
-// 	});
-// });
+	client.query(query, (err, data) => {
+		if (err) {
+			res.status(400).send({ status: 400, error: err });
+		}
+		res.status(200).send(data.rows);
+	});
+});
 
 // // Update
 // // recieve put request from client
-// app.put("/todos/:todoid", (req, res) => {
-// 	// recieve id from url
-// 	let id = req.params.todoid;
-// 	// use id to find the document
-// 	TodoModel.findById(id, (error, result) => {
-// 		if (error) {
-// 			console.log("Error finding documents from database: ", error);
-// 			// receive data from database
-// 		} else {
-// 			// toggole isComplete field's boolean value
-// 			result.isComplete = !result.isComplete;
-// 			// save result to server
-// 			result.save((error, resultAfterSave) => {
-// 				if (error) {
-// 					console.log("There was an error saving to db");
-// 				} else {
-// 					console.log("Data is suceessfully saved:", result);
-// 					// if save data to database, then
-// 					// get the result from the sever and send it to client
-// 					// ?? is the
-// 					res.status(201).json(resultAfterSave);
-// 				}
-// 			});
-// 		}
-// 	});
-// });
+app.put("/todos/:todoid", (req, res) => {
+	// recieve id from url
+	let id = parseInt(req.params.todoid);
+	// use id to find the row
+	let query = `
+			UPDATE todos.todos
+			SET iscomplete = NOT iscomplete
+			WHERE id = ${id}
+			RETURNING id as _id, *;
+		`;
+	client.query(query, (err, data) => {
+		if (err) {
+			res.status(400).send({ status: 400, error: err });
+		}
+		res.status(200).send(data.rows[0]);
+	});
+});
 
-// // DELETE
-// // when recieve delete request from client
-// app.delete("/todos/:todoid", (req, res) => {
-// 	let id = req.params.todoid;
-// 	// send delete request to database with id
-// 	TodoModel.findByIdAndDelete(id, (error, result) => {
-// 		if (error) {
-// 			console.log("Error finding documents from database: ", error);
-// 			// receive data from database
-// 		} else {
-// 			// send result to client
-// 			// ??? could we just send the key instead of sending the whole information about the user ???
-// 			console.log(result);
-// 			res.status(201).json(result);
-// 		}
-// 	});
-// });
+// DELETE
+// when recieve delete request from client
+app.delete("/todos/:todoid", (req, res) => {
+	let id = parseInt(req.params.todoid);
+	let query = `
+			DELETE FROM todos.todos
+			WHERE id = ${id}
+			RETURNING id as _id, *;
+		`;
+	client
+		.query(query)
+		.then(function (data) {
+			res.send(data.rows[0]);
+		})
+		.catch(function (err) {
+			res.json({ code: 400, message: err.stack, err });
+		});
+});
+//
 
-// const PORT = process.env.PORT || 3000;
-// app.listen(PORT, () => {
-// 	console.log(`App is listening on PORT ${PORT}.`);
-// });
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+	console.log(`App is listening on PORT ${PORT}.`);
+});
