1c451fa1590dc2a9914024c5a7624daeb28ef706
seaboyz <seaboyz@hotmail.com>
2020-09-25 15:04:47 -0500

    change folder name

--- 09_NodeJS_APIs/Project1-node-fetch-form/app.js
+++ 09_NodeJS_APIs/Project1-node-fetch-form/app.js
@@ -9,29 +9,47 @@ app.listen(PORT, () => console.log(`App is listening on port ${PORT}`));
 
 app.use(express.static("public"));
 
-
 app.get("/", (req, res) => {
-    res.render("index.ejs");
+	res.render("index.ejs");
 });
 
 app.get("/getPrice", (req, res) => {
-    // fetch from coindesk
-    fetch("https://api.coindesk.com/v1/bpi/currentprice.json")
-        .then(response => {
-            if(!response){
-                throw Error("no response")
-            }
-            return response.json()
-        })
-        .then(data => {
-            let currency = req.query.currency.toUpperCase();
-            let unit = data.bpi[currency].symbol;
-            let price = data.bpi[currency].rate_float.toFixed(2);
-            res.render("price.ejs", {
-                price: price,
-                unit: unit,
-            });
-        });
+	// fetch from coindesk
+	fetch("https://api.coindesk.com/v1/bpi/currentprice.json")
+		.then(response => {
+			if (!response) {
+				throw Error("no response");
+			}
+			return response.json();
+		})
+		.then(data => {
+			let currency = req.query.currency.toUpperCase();
+			let unit = data.bpi[currency].symbol;
+			let price = data.bpi[currency].rate_float.toFixed(2);
+			res.render("price.ejs", {
+				price: price,
+				unit: unit,
+			});
+		});
 });
 
-
+app.post("/getPrice", (req, res) => {
+	console.log(req);
+	// fetch from coindesk
+	// fetch("https://api.coindesk.com/v1/bpi/currentprice.json")
+	// 	.then(response => {
+	// 		if (!response) {
+	// 			throw Error("no response");
+	// 		}
+	// 		return response.json();
+	// 	})
+	// 	.then(data => {
+	// 		let currency = req.body.currency.toUpperCase();
+	// 		let unit = data.bpi[currency].symbol;
+	// 		let price = data.bpi[currency].rate_float.toFixed(2);
+	// 		res.render("price.ejs", {
+	// 			price: price,
+	// 			unit: unit,
+	// 		});
+	// 	});
+});
