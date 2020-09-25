a06d5c9319e5f951cc1b419da8e8d1ae165925f1
seaboyz <seaboyz@hotmail.com>
2020-09-25 15:08:15 -0500

    Commenting

--- 09_NodeJS_APIs/Project1-node-fetch-form/app.js
+++ 09_NodeJS_APIs/Project1-node-fetch-form/app.js
@@ -2,12 +2,15 @@ const express = require("express");
 
 const fetch = require("node-fetch");
 
+const bodyParser = require("body-parser");
+
 const app = express();
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`App is listening on port ${PORT}`));
 
 app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get("/", (req, res) => {
 	res.render("index.ejs");
@@ -33,23 +36,23 @@ app.get("/getPrice", (req, res) => {
 		});
 });
 
+// use post method for the same result
 app.post("/getPrice", (req, res) => {
-	console.log(req);
 	// fetch from coindesk
-	// fetch("https://api.coindesk.com/v1/bpi/currentprice.json")
-	// 	.then(response => {
-	// 		if (!response) {
-	// 			throw Error("no response");
-	// 		}
-	// 		return response.json();
-	// 	})
-	// 	.then(data => {
-	// 		let currency = req.body.currency.toUpperCase();
-	// 		let unit = data.bpi[currency].symbol;
-	// 		let price = data.bpi[currency].rate_float.toFixed(2);
-	// 		res.render("price.ejs", {
-	// 			price: price,
-	// 			unit: unit,
-	// 		});
-	// 	});
+	fetch("https://api.coindesk.com/v1/bpi/currentprice.json")
+		.then(response => {
+			if (!response) {
+				throw Error("no response");
+			}
+			return response.json();
+		})
+		.then(data => {
+			let currency = req.body.currency.toUpperCase();
+			let unit = data.bpi[currency].symbol;
+			let price = data.bpi[currency].rate_float.toFixed(2);
+			res.render("price.ejs", {
+				price: price,
+				unit: unit,
+			});
+		});
 });
