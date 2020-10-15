8b350a884481bb03c8f12d7cbbf92fca9857af77
seaboyz <seaboyz@hotmail.com>
2020-10-14 22:20:03 -0500

    Use function component instead of class component

--- Practice/recipe-props/src/Recipe.js
+++ Practice/recipe-props/src/Recipe.js
@@ -1,27 +1,50 @@
-import React, { Component } from 'react'
+// import React, { Component } from 'react'
 import './Recipe.css'
 
-class Recipe extends Component {
-  render() {
-    const { title, img, instructions } = this.props
-    const ingredients = this.props.ingredients.map((ingredient, index) => (
-      <li key={index}>{ingredient}</li>
-    ))
-    return (
-      <div className='recipe-card'>
-        <div className='recipe-card-image'>
-          <img src={img} alt={title}></img>
-        </div>
-        <div className='recipe-card-content'>
-          <h3 className='recipe-title'>{title}</h3>
-          <h4>Ingredients:</h4>
-        </div>
-        <ul>{ingredients}</ul>
-        <h4>instructions:</h4>
-        <p>{instructions}</p>
+import React from 'react'
+
+const Recipe = props => {
+  const { title, img, instructions } = props
+  const ingredients = props.ingredients.map((ingredient, index) => (
+    <li key={index}>{ingredient}</li>
+  ))
+  return (
+    <div className='recipe-card'>
+      <div className='recipe-card-image'>
+        <img src={img} alt={title}></img>
       </div>
-    )
-  }
+      <div className='recipe-card-content'>
+        <h3 className='recipe-title'>{title}</h3>
+        <h4>Ingredients:</h4>
+      </div>
+      <ul>{ingredients}</ul>
+      <h4>instructions:</h4>
+      <p>{instructions}</p>
+    </div>
+  )
 }
+
+// class Recipe extends Component {
+//   render() {
+//     const { title, img, instructions } = this.props
+//     const ingredients = this.props.ingredients.map((ingredient, index) => (
+//       <li key={index}>{ingredient}</li>
+//     ))
+//     return (
+//       <div className='recipe-card'>
+//         <div className='recipe-card-image'>
+//           <img src={img} alt={title}></img>
+//         </div>
+//         <div className='recipe-card-content'>
+//           <h3 className='recipe-title'>{title}</h3>
+//           <h4>Ingredients:</h4>
+//         </div>
+//         <ul>{ingredients}</ul>
+//         <h4>instructions:</h4>
+//         <p>{instructions}</p>
+//       </div>
+//     )
+//   }
+// }
 
 export default Recipe
