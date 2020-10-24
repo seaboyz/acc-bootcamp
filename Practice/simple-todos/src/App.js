f573cebeff55075a6d9613685bda0c2a79b33e04
seaboyz <seaboyz@hotmail.com>
2020-10-23 23:19:43 -0500

    Add handleComplete event handler

--- Practice/simple-todos/src/App.js
+++ Practice/simple-todos/src/App.js
@@ -34,32 +34,140 @@ class App extends Component {
     this.setState({ todos, newTodo: '' })
   }
 
-  // handleComplete(event) {
-  //   // use setState to change the state
-  //   // pass a callback function to copy the state and change it and
-  //   // return it
-  //   let id = event.target.id
-  //   this.setState(function (cloneOfState) {
-  //     let indexOfTodo = cloneOfState.todos.findIndex(
-  //       todo => todo.id === parseInt(id)
-  //     )
-  //     let foundTodo = cloneOfState.todos[indexOfTodo]
-  //     foundTodo.isComplete = !foundTodo.isComplete
-  //     return foundTodo
-  //   })
-  // }
+  /* ****************************************** */
+  // no copy
+  // using updater funtion
+  handleComplete(event) {
+    let id = event.target.id
+    this.setState(
+      // state is a reference to the component state at the time the change is being applied. It should not be directly mutated. Instead, changes should be represented by building a new object based on the input from state and props.
+      // without building a new object, updater funtion at the strict mode will run twice, cause boolean toggle back to what it was.
+      // updater funtion should be pure. without side effect.
+      function (copyOfState) {
+        let indexOfTodo = copyOfState.todos.findIndex(
+          todo => todo.id === parseInt(id)
+        )
+        console.log('previous state', copyOfState)
+        let foundTodo = copyOfState.todos[indexOfTodo]
+        console.log('foundTodo', foundTodo)
+        foundTodo.isComplete = !foundTodo.isComplete
+        console.log('current state', copyOfState)
+        console.log(this.state)
+        return copyOfState
+      },
+      () => {
+        console.log('this.state', this.state)
+      }
+    )
+  }
+
+  /* ****************************************** */
+  /*   // shallow copy changing the original state(change todo array )
+  // using updater funtion
+  handleComplete(event) {
+    let id = event.target.id
+    this.setState(
+      function (state) {
+        // let cloneOfState = { ...this.state }
+        let cloneOfState = Object.assign({}, state)
+        let indexOfTodo = cloneOfState.todos.findIndex(
+          todo => todo.id === parseInt(id)
+        )
+        let foundTodo = cloneOfState.todos[indexOfTodo]
+        console.log('state', state)
+        foundTodo.isComplete = !foundTodo.isComplete
+        console.log('state', state)
+        console.log('cloneOfState', cloneOfState)
+        return cloneOfState
+      },
+      () => {
+        console.log('current state', this.state)
+      }
+    )
+  } */
+
+  /* ****************************************** */
+  /*  // deep copy changing the original state(change todo array )
+  // using updater funtion
+  handleComplete(event) {
+    let id = event.target.id
+    this.setState(
+      function (state) {
+        // let cloneOfState = { ...this.state }
+        let cloneOfState = JSON.parse(JSON.stringify(this.state))
+        let indexOfTodo = cloneOfState.todos.findIndex(
+          todo => todo.id === parseInt(id)
+        )
+        let foundTodo = cloneOfState.todos[indexOfTodo]
+        foundTodo.isComplete = !foundTodo.isComplete
+        console.log('previous state', state)
+        console.log('cloneOfState', cloneOfState)
+        return cloneOfState
+      },
+      () => {
+        console.log('current state', this.state)
+      }
+    )
+  } */
+
+  /* ****************************************** */
+  /*   // shallow copy changing the original state(change todo array )
+  // using updater funtion
+  handleComplete(event) {
+    let id = event.target.id
+    this.setState(function (state) {
+      // let cloneOfState = { ...this.state }
+      let cloneOfState = Object.assign({}, state)//this is just a shawlow copy
+      let indexOfTodo = cloneOfState.todos.findIndex(
+        todo => todo.id === parseInt(id)
+      )
+      let foundTodo = cloneOfState.todos[indexOfTodo]
+      foundTodo.isComplete = !foundTodo.isComplete
+      console.log('previous state', state)
+      console.log('cloneOfState', cloneOfState)
+      return cloneOfState
+    },() => {
+        console.log('current state', this.state)
+      })
+  } */
+
+  /* ****************************************** */
+  /*  // shallow copy changing the original state(change todo array )
+  // use updater object
   handleComplete(event) {
     // use setState to change the state
     // pass updated object to update the state
     let id = event.target.id
-    let indexOfTodo = this.state.todos.findIndex(
+    // let cloneOfState = { ...this.state }
+    let cloneOfState = Object.assign({}, this.state)
+    let indexOfTodo = cloneOfState.todos.findIndex(
       todo => todo.id === parseInt(id)
     )
-    let foundTodo = this.state.todos[indexOfTodo]
+    let foundTodo = cloneOfState.todos[indexOfTodo]
     foundTodo.isComplete = !foundTodo.isComplete
-    console.log(foundTodo)
-    this.setState(foundTodo)
-  }
+    console.log('previous state', this.state)
+    console.log('cloneOfState', cloneOfState)
+    this.setState(cloneOfState, () => {
+      console.log('current state', this.state)
+    })
+  } */
+  /* ****************************************** */
+  /* // deep copy to clone of state
+// without changing the original state
+  handleComplete(event) {
+    // use setState to change the state
+    // pass updated object to update the state
+    let id = event.target.id
+    let cloneOfState = JSON.parse(JSON.stringify(this.state))
+    let indexOfTodo = cloneOfState.todos.findIndex(
+      todo => todo.id === parseInt(id)
+    )
+    let foundTodo = cloneOfState.todos[indexOfTodo]
+    foundTodo.isComplete = !foundTodo.isComplete
+    console.log('previous state', this.state)
+    console.log('cloneOfState', cloneOfState)
+    this.setState(cloneOfState, () => console.log("current state",this.state))
+  } */
 
   render() {
     return (
--- Practice/simple-todos/src/index.js
+++ Practice/simple-todos/src/index.js
@@ -5,9 +5,12 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  // <React.StrictMode>
+  //   <App />
+  // </React.StrictMode>,
+
+    <App />,
+
   document.getElementById('root')
 );
 
